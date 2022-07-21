a32e2ff10b17a9a104e67b45bda3d37474b341af
BastianAhumada <bastianahumadajimenez@gmail.com>
2022-07-20 21:41:06 -0400

    Create first steps to function

--- functions/hello/index.js
+++ functions/hello/index.js
@@ -1,4 +1,33 @@
-module.exports = () => {
-    return console.log('Hola mundo')
-    
+module.exports = (event, _, callback) => {
+
+  if (!event.headers.authorization) {
+    callback(null, {
+      error: {
+        code: "UNATHORIZED",
+        message: "Authorization is Required"
+      }
+    })
+  }
+  
+  if (!event.body) {
+    callback(null, {
+      error: {
+        code: "MISSING PARAMETER",
+        message: "Body is Required"
+      }
+    })
+  }
+
+  console.log('Body', event.body)
+
+  const {firstName, lastName, Mascota, nameMascota } = JSON.parse(event.body)
+  
+  if (event.body) {
+    callback(null, {
+      error: {
+        code: "MISSING PARAMETER",
+        message: "Parameters `firstName`, `lastName`, `Mascota`, `nameMascota` is Required"
+      }
+    })
+  }
 }
